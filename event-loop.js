a8dff22cf6bed8c259328b3ccf7ae3895ec09959
Poran chowdury <chowdury659@gmail.com>
2020-09-13 13:07:03 +0600

    Initial commit

---
+++ event-loop.js
@@ -0,0 +1,22 @@
+const fs  = require('fs');
+const crypto = require('crypto');
+setTimeout(() =>{
+    console.log('timer 1 is finished ')
+})
+setImmediate(() =>{
+console.log("immediate timer 1 is finished")
+})
+
+fs.readFile('test.txt', ((err, data) => {
+    console.log('i/o task completed')
+    setTimeout(() =>{
+        console.log('timer 2 is finished ')
+    })
+    setTimeout(() =>{
+        console.log('timer 3 is finished ')
+    },3000)
+    setImmediate(() =>{
+        console.log("immediate timer 2 is finished")
+    })
+}))
+console.log('hello from top level code !');
